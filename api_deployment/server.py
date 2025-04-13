7270d23fb73ecd48e1ccef1c7ace3e86c6033ad9
John Stanwick <48192612+jwstanwick@users.noreply.github.com>
2025-04-13 10:16:26 -0400

    updated port

--- api_deployment/server.py
+++ api_deployment/server.py
@@ -314,4 +314,4 @@ async def health_check():
     return {"status": "healthy"}
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
